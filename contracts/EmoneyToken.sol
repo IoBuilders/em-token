d554e0ba50c4429fc5b2217a11f3efa33bd74163
Daniel Lehrner <daniel@io.builders>
2020-01-13 15:20:24 +0100

    added operator send function, updated dependencies

--- contracts/EmoneyToken.sol
+++ contracts/EmoneyToken.sol
@@ -6,5 +6,16 @@ import "./IHoldable.sol";
 import "./INativeHoldable.sol";
 
 contract EmoneyToken is IERC20, ITwoStepTransferableToken, IHoldable, INativeHoldable {
+    function isTransferOperatorFor(address _operator, address _from) external view returns (bool);
 
+    function authorizeTransferOperator(address _operator) external returns (bool);
+
+    function revokeTransferOperator(address _operator) external returns (bool);
+
+    function operatorSend(
+        string calldata _operationId,
+        address _from,
+        address _to,
+        uint256 _value
+    ) external returns (bool);
 }
